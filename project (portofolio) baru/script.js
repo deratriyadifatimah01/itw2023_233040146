481a90d6c711c8d09313a7308f974833fba1ecbf
deratriyadifatimah01 <147587092+deratriyadifatimah01@users.noreply.github.com>
2023-12-23 13:13:47 +0700

    Delete project (portofolio) baru directory

--- project (portofolio) baru/script.js
+++
@@ -1,6 +0,0 @@
-const menuTonggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
-
-menuTonggle.addEventListener('click', function () {
-    nav.classList.toggle('slide');
-});
